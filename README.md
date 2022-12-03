35cd43906c64e965d637662a26b273f3d0f9d96f
grant horwood <ghorwood@cloverhitch.ca>
2022-12-03 16:51:17 -0700

    initial commit from bitbucket
    Signed-off-by: grant horwood <ghorwood@cloverhitch.ca>

---
+++ README.md
@@ -0,0 +1,54 @@
+Pollinate is a command for `artisan` for use in Laravel projects that creates database seed files from your database. If you have data in a database and want to turn it into seeds, pollinate is here.
+
+## Install
+To install manually, download the the `pollinate.php` file to `app/Console/Commands` then register in `Commands\Pollinate::class` in the `commands` array in `app/Console/Kernel.php`.
+
+For a quicker install, run:
+
+`php artisan make:command pollinate && curl -L https://raw.githubusercontent.com/gbhorwood/pollinate/master/src/pollinate.php -o app/Console/Commands/pollinate.php`
+
+## Prerequisites
+Pollinate requires the following:
+
+* PHP 7.4 or higher
+* The `pdo` extension
+* The composer package `doctrine/dbal`
+
+## Usage
+The simplest usage of pollinate is:
+
+```
+php artisan gbhorwood:pollinate
+```
+
+This will create seed files for _all_ the tables in your database, and all seed files and classnames will be prefixed with the default `pollinate_` prefix.
+
+### pollinating specific tables
+If you only wish to create seed files for specified tables, you can provide a comma-separated list of table names as an argument:
+
+```
+php artisan gbhorwood:pollinate users,pets,user_pet
+```
+
+### specifying a prefix
+By default, pollinate prefixes all seed file and class names with `pollinate_`. You can specify your own prefix with the `--prefix=` option:
+
+```
+php artisan gbhorwood:pollinate --prefix=mydevbox
+```
+
+this will create seed files and classes with names like `mydevbox_Pets.php`.
+
+### overwriting existing seed files
+Pollinate's default behaviour is to not overwrite existing seed files. If one or more seed files exist that would be overwritten, pollinate will error and quit before doing any work.
+
+If you wish to override this behaviour so that existing seed files are overwritten, you can pass the `--overwrite` option:
+
+```
+php artisan gbhorwood:pollinate --overwrite
+```
+
+### missing or empty tables
+Pollinate does not write seed files for missing or empty tables.
+
+
---
+++ pollinate.php
@@ -0,0 +1,638 @@
+<?php
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2019 grant horwood
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace App\Console\Commands;
+
+use DB;
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+
+/**
+ * Page size for database selects
+ */
+define('POLLINATE_SELECT_SIZE', 5);
+
+class pollinate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gbhorwood:pollinate
+        {--prefix= : Prefix to file and class names. Default \'pollinate\'}
+        {--overwrite : Overwrite existing seeder files of the same name}
+        {--silent : Suppress non-error output}
+        {tables?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create seed files from the database
+  Accepts an optional comma-separated list of tables to seed.
+  If no tables list is provided, all tables seeded.';
+
+    /**
+     * Array of table names we do not create seeds for
+     * Modify as desired.
+     *
+     * @var Array
+     */
+    protected Array $ignoreTables = [
+        'jobs',
+        'failed_jobs',
+        'oauth_access_tokens',
+        'oauth_auth_codes',
+        'oauth_clients',
+        'oauth_personal_access_clients',
+        'oauth_refresh_tokens',
+        'password_resets',
+        'personal_access_tokens',
+    ];
+
+    /**
+     * Display non-error output
+     *
+     * @var bool
+     */
+    protected bool $showOutput = true;
+
+    /**
+     * Prefix for seeder file and class names
+     *
+     * @var String
+     */
+    protected String $prefix = "pollinate";
+
+    /**
+     * Overwrite seed files if true
+     *
+     * @var bool
+     */
+    protected bool $overwrite = false;
+
+    /**
+     * The full path of the directory where seeder files 
+     * will be written
+     *
+     * @var String
+     */
+    protected String $seedsDirectory;
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle():Int
+    {
+        /**
+         * Set and handle command line options
+         */
+        $this->handleArgs();
+
+        /**
+         * Deduce target directory for writing seeds
+         */
+        $this->seedsDirectory = $this->getSeedsDirectory();
+
+        /**
+         * Validate we can run this script
+         */
+        $this->preflight();
+
+        /**
+         * Create list of tables to build seeds for
+         */
+        $tableNames = $this->getTableNames();
+
+        /**
+         * Handle desired overwrite behaviour; either delete existing target files or error
+         */
+        $this->handleOverwrite($tableNames);
+
+        /**
+         * Write seed file for each table
+         */
+        $seedClasses = array_map(fn($tn) => $this->writeSeed($tn), $tableNames);
+
+        /**
+         * Output content for DatabaseSeeder.php
+         */
+        if($this->showOutput) {
+            $this->info(PHP_EOL."Add this to DatabaseSeeder.php");
+            array_map(fn($sc) => print $sc.','.PHP_EOL, array_filter($seedClasses));
+        }
+
+        return 0;
+    }
+
+
+    /**
+     * Write one seed file for table $tablename
+     *
+     * @param  String $tablename
+     * @return String The class name to add to DatabaseSeeder.php
+     * @throws Exception
+     */
+    private function writeSeed(String $tablename)
+    {
+        $fileName = $this->getFileName($tablename);
+        $className = $this->getClassName($tablename);
+        $seederFileHead = $this->getSeederFileHead($tablename, $className);
+        $seederFileFoot = $this->getSeederFileFoot();
+
+        try{
+            /**
+             * Write head of seed file
+             */
+            $fp = fopen($fileName, 'a');
+            fwrite($fp, $seederFileHead);
+
+            /**
+             * Write records by page
+             */
+            foreach($this->select($tablename) as $recordSet) {
+                if(count($recordSet) > 0) {
+                    $formattedRecordSet = $this->formatRecordSet($recordSet);
+                    fwrite($fp, $formattedRecordSet);
+                }
+            }
+
+            /**
+             * Write foot of seed file and close
+             */
+            fwrite($fp, $seederFileFoot);
+            fclose($fp);
+
+            $this->doInfo("Seeded table '$tablename'");
+
+            /**
+             * Return class name for DatabaseSeeder.php
+             */
+            return $className.'::class';
+        }
+        /**
+         * Any error writing the seed file results in deletion of the file
+         */
+        catch(\Exception $e) {
+            fclose($fp);
+            unlink($fileName);
+        }
+    }
+
+
+    /**
+     * Confirm the system can run this script. Exit on
+     * any failure.
+     * 
+     * @return void
+     */
+    private function preflight():void
+    {
+        /**
+         * Confrim minimum PHP version
+         */
+        $phpversion_array = explode('.', phpversion());
+        if ((int)$phpversion_array[0].$phpversion_array[1] < 74) {
+            $this->error("PHP must be 7.4 or higher");
+            die();
+        }
+
+        /**
+         * Confirm necessary extensions
+         */
+        if(!extension_loaded('pdo')) {
+            $this->error("pdo PHP extension must be loaded");
+            die();
+        }
+
+        /**
+         * Confirm target directory is writeable
+         */
+        if(!is_writeable($this->seedsDirectory)) {
+            $this->error("Cannot write to ".$this->seedsDirectory);
+            die();
+        }
+
+        /**
+         * Confirm doctrine/dbal is installed
+         */
+        if(!in_array('doctrine/dbal', \Composer\InstalledVersions::getInstalledPackages())) {
+            $this->error("The doctrine/dbal package must be installed");
+            fwrite(STDERR, "run:".PHP_EOL);
+            fwrite(STDERR, "composer require doctrine/dbal");
+            die();
+        }
+        
+    }
+
+
+    /**
+     * Get the seeder stub from Laravel as array keyed by 'head' and 'foot'.
+     * Head is everything above where the seed records go, foot is everything below
+     *
+     * @return Array
+     */
+    private function getStubParts():Array
+    {
+        $stubPath = $this->laravel->basePath()."/vendor/laravel/framework/src/Illuminate/Database/Console/Seeds/stubs/seeder.stub";
+        return array_combine(['head', 'foot'], explode('//', file_get_contents($stubPath)));
+    }
+
+
+    /**
+     * Get the directory where the seeders live. One of:
+     *  - database/seeders
+     *  - database/seeds
+     * If neither exist, error and die.
+     *
+     * @return String
+     */
+    private function getSeedsDirectory():String
+    {
+        /**
+         * Potential valid seeder directories
+         */
+        $possibleDirectories = [
+            $this->laravel->basePath().DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'seeders',
+            $this->laravel->basePath().DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'seeds',
+        ];
+
+        /**
+         * Return the first valid directory
+         */
+        foreach($possibleDirectories as $possibleDirectory) {
+            if(file_exists($possibleDirectory) && is_dir($possibleDirectory)) {
+                return $possibleDirectory;
+            }
+        }
+
+        $errorMessage = PHP_EOL."No valid seeder directory at:".join(array_map(fn($p) => PHP_EOL."* $p ", $possibleDirectories));
+        $this->error($errorMessage);
+        die();
+    }
+
+
+    /**
+     * Get the modified seed file stub above the seed array
+     * 
+     * @param  String $tablename
+     * @param  String $className
+     * @return String
+     */
+    private function getSeederFileHead(String $tablename, String $className):String
+    {
+        $stubHead = trim($this->getStubParts()['head']).PHP_EOL.PHP_EOL;
+        $stubHead = str_replace("{{ class }}", $className, $stubHead);
+        $stubHead = $stubHead.$this->getDocblock($tablename);
+        $stubHead = $stubHead.PHP_EOL.$this->indent(2)."\Schema::disableForeignKeyConstraints();".PHP_EOL;
+        $stubHead = $stubHead.PHP_EOL.$this->indent(2)."\DB::table('$tablename')->delete();".PHP_EOL;
+        $stubHead = $stubHead.PHP_EOL.$this->indent(2)."\DB::table('$tablename')->insert(".PHP_EOL;
+        return $stubHead;
+    }
+
+
+    /**
+     * Get the modified seed file stub below the seed array
+     *
+     * @return String
+     */
+    private function getSeederFileFoot():String
+    {
+        $stubFoot = "";
+        $stubFoot .= PHP_EOL.$this->indent(2).");".PHP_EOL;
+        $stubFoot .= PHP_EOL.$this->indent(2)."\Schema::enableForeignKeyConstraints();";
+        $stubFoot .= PHP_EOL.$this->getStubParts()['foot'];
+        return $stubFoot;
+    }
+
+
+    /**
+     * Handle overwrite option. If overwriting, delete all existing files
+     * we will be creating. If not overwriting, test for existing files
+     * we will be creating and error if existing.
+     *
+     * @param  Array $tableNames
+     * @return void
+     */
+    private function handleOverwrite(Array $tableNames):void
+    {
+        /**
+         * Overwrite existing seeder files
+         * Pre delete all files we will be creating later, if any
+         */
+        if($this->overwrite) {
+            array_map(function($t) {
+                $path = $this->getFileName($t);
+                if(file_exists($path)) {
+                    if(unlink($path)) {
+                        $this->doInfo("deleted $path");
+                    }
+                    else {
+                        $this->error("could not delete $path");
+                        die();
+                    }
+                }
+            }, $tableNames);
+        }
+
+        /**
+         * Do not overwrite existing seeder files
+         * If any files we will be creating later exist, error and die
+         */
+        if(!$this->overwrite) {
+            $existingFiles = array_map(function($t) {
+                $path = $this->getFileName($t);
+                if(file_exists($path)) {
+                    return $path;
+                }
+            }, $tableNames);
+            $existingFiles = array_filter($existingFiles);
+
+            if(count($existingFiles)) {
+                $errorMessage = PHP_EOL."Cannot overwrite the following files:".join(array_map(fn($t) => PHP_EOL."* $t ", $existingFiles));
+                $this->error($errorMessage);
+                fwrite(STDERR, "You can force overwrite by passing the --overwrite option.");
+            }
+        }
+    }
+
+
+    /**
+     * Format an array of records selected from a table into php array 
+     * notation, indented properly for a seeder file.
+     *
+     * @param  Array $recordSet The array from a DB::select()
+     * @return String
+     */
+    private function formatRecordSet(Array $recordSet):String
+    {
+        /**
+         * Function to format value as number, null or string
+         */
+        $formatValue = function($value) {
+            if($value == null) {
+                return 'null';
+            }
+            return is_numeric($value) ? $value : "'".addslashes($value)."'";
+        };
+
+        /**
+         * For each record set, create one record block and return as
+         * array element
+         */
+        $formattedRecordSetArray = array_map(function($r) use($formatValue) {
+            $buffer = null;
+            foreach($r as $k => $v) {
+                $buffer .= $this->indent(4)."'$k' => ".$formatValue($v).",".PHP_EOL;
+            }
+            return $this->indent(3).'['.PHP_EOL.$buffer.$this->indent(3).'],'.PHP_EOL;
+        }, $recordSet);
+
+        /**
+         * Return array of formatted record blocks as string
+         */
+        return join($formattedRecordSetArray);
+    }
+
+
+    /**
+     * Generator to yield one page of records from the specified
+     * table. Terminates when no more records.
+     *
+     * @param  String $tablename
+     */
+    private function select(String $tablename)
+    {
+        $page = 0;
+
+        do{
+            $limit = POLLINATE_SELECT_SIZE;
+            $offset = POLLINATE_SELECT_SIZE * $page;
+
+            $sql =<<<SQL
+                SELECT  *
+                FROM    $tablename
+                LIMIT   $limit
+                OFFSET  $offset
+            SQL;
+
+            try {
+                $result = DB::select($sql);
+            }
+            catch(\Exception $e) {
+                $this->error("Table '$tablename' does not exist or is empty. Seed not written.");
+                throw new \Exception();
+            }
+            yield $result;
+
+            $page++;
+
+        }while(count($result) > 0);
+    }
+
+
+    /**
+     * Get array of all table names
+     * Requires doctrine/dbal
+     *
+     * @return Array
+     */
+    private function getTableNames():Array
+    {
+        /**
+         * Table names provided as argument
+         */
+        if($this->argument('tables')) {
+            return explode(',', $this->argument('tables'));
+        }
+
+        /**
+         * Get all table names through DB for portability
+         */
+        $tablesAll = \DB::connection()
+            ->getDoctrineSchemaManager()
+            ->listTableNames();
+
+        /**
+         * Filter ignored tables
+         */
+        return array_values(array_filter($tablesAll, fn($t) => !in_array($t, $this->ignoreTables)));
+    }
+
+
+    /**
+     * Returns the doc block to put at the top of the seeder file
+     *
+     * @param  String $tablename
+     * @return String
+     */
+    private function getDocblock(String $tablename):String
+    {
+        return $this->indent(2).'/**'.PHP_EOL.
+               $this->indent(2).' * Created by pollinate.'.PHP_EOL.
+               $this->indent(2).' * '.PHP_EOL.
+               $this->indent(2).' * Table: '.$this->getDatabaseName().'.'.$tablename.PHP_EOL.
+               $this->indent(2).' * User:  '.$this->getUsername().PHP_EOL.
+               $this->indent(2).' * Host:  '.$this->getHostname().PHP_EOL.
+               $this->indent(2).' * Date:  '.$this->getNow().PHP_EOL.
+               $this->indent(2).' * Env:   '.$this->getEnvironment().PHP_EOL.
+               $this->indent(2).' */'.PHP_EOL;
+    }
+
+
+    /**
+     * Get indentation string of spaces to prepend for indentation.
+     * 'Tab stops'[sic.] are four spaces.
+     *
+     * @param  Int $tabStops The number of 'tabs' to indent, ie four-space strings
+     * @return String The string of spaces to prepend
+     */
+    private function indent(Int $tabStops):String
+    {
+        return str_repeat(' ', $tabStops * 4);
+    }
+
+
+    /**
+     * Get username of executing user
+     * 
+     * @return String
+     */
+    private function getUsername():String
+    {
+        return extension_loaded('posix') ? posix_getpwuid(posix_geteuid())['name'] : get_current_user();
+    }
+
+
+    /**
+     * Get hostname
+     * 
+     * @return String
+     */
+    private function getHostname():String
+    {
+        return gethostname() ?? '';
+    }
+
+
+    /**
+     * Get the name of the database for the default db
+     * as set in config.database 
+     * 
+     * @return String
+     */
+    private function getDatabaseName():String
+    {
+        return config('database.connections.'.config('database.default').'.database');
+    }
+
+
+    /**
+     * Get the environment, ie. 'local' or 'production'
+     *
+     * @return String
+     */
+    private function getEnvironment():String
+    {
+        return config('app.env') ?? 'local';
+    }
+
+
+    /**
+     * Get current date and time with timezone in format
+     * YYYY-MM-DD HH:ii:ss TZ
+     *
+     * @return String
+     */
+    private function getNow():String
+    {
+        return date("Y-m-d H:i:s e");
+    }
+
+
+    /**
+     * Get the name of the class for a tablename's seeder
+     *
+     * @param  String $tablename
+     * @return String
+     */
+    private function getClassName(String $tablename):String
+    {
+        return $this->prefix.'_'.Str::ucfirst(Str::camel($tablename));
+    }
+
+
+    /**
+     * Get the name of the file for a tablename's seeder
+     *
+     * @param  String $tablename
+     * @return String
+     */
+    private function getFileName(String $tablename):String
+    {
+        return $this->seedsDirectory.DIRECTORY_SEPARATOR.$this->getClassName($tablename).".php";
+    }
+
+
+    /**
+     * Call info(). Suppress if --silent or --quiet is set
+     *
+     * @param  String $message
+     * @return void
+     */
+    private function doInfo(String $message):void
+    {
+        $this->showOutput ? $this->info($message) : null;
+    }
+
+
+    /**
+     * Parse arguments
+     * 
+     * @return void
+     */
+    private function handleArgs():void
+    {
+        $this->showOutput = (bool)$this->option('silent') || (bool)$this->option('quiet') ? false : true;
+        $this->prefix = $this->option('prefix') ?? 'pollinate';
+        $this->overwrite = (bool)$this->option('overwrite') ? true : false;
+    }
+}
